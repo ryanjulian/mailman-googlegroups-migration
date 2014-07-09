cbe5286571cc6fd7723734df4486cd45ad1af618
Ryan Julian <ryanjulian@pioneers.berkeley.edu>
2014-07-08 23:25:13 -0700

    Fix logging and add first stab at uploads

--- migrate.py
+++ migrate.py
@@ -49,14 +49,14 @@ CLIENT_SECRETS = 'client_secrets.json'
 
 # CREDENTIAL_FILE, name of a file where the script will automatically store
 # OAuth 2.0 credentials obtained during the authorization process.
-CREDENTIAL_FILE = 'migrate_credentials.dat'
+CREDENTIAL_FILE = 'credentials.dat'
 
 # Helpful message to display in the browser if the CLIENT_SECRETS file
 # is missing.
 MISSING_CLIENT_SECRETS_MESSAGE = """
 WARNING: Please configure OAuth 2.0
 
-To make this sample run you will need to populate the client_secrets.json file
+To make this script run you will need to populate the client_secrets.json file
 found at:
 
    %s
@@ -66,44 +66,6 @@ with information from the APIs Console <https://code.google.com/apis/console>.
 """ % os.path.join(os.path.dirname(__file__), CLIENT_SECRETS)
 
 
-def access_settings(service, groupId, settings):
-  """Retrieves a group's settings and updates the access permissions to it.
-
-  Args:
-    service: object service for the Group Settings API.
-    groupId: string identifier of the group@domain.
-    settings: dictionary key-value pairs of properties of group.
-  """
-
-  # Get the resource 'group' from the set of resources of the API.
-  # The Group Settings API has only one resource 'group'.
-  group = service.groups()
-
-  # Retrieve the group properties
-  g = group.get(groupUniqueId=groupId).execute()
-  print '\nGroup properties for group %s\n' % g['name']
-  pprint.pprint(g)
-
-  # If dictionary is empty, return without updating the properties.
-  if not settings.keys():
-    print '\nGive access parameters to update group access permissions\n'
-    return
-
-  body = {}
-
-  # Settings might contain null value for some keys(properties). 
-  # Extract the properties with values and add to dictionary body.
-  for key in settings.iterkeys():
-    if settings[key] is not None:
-      body[key] = settings[key]
-
-  # Update the properties of group
-  g1 = group.update(groupUniqueId=groupId, body=body).execute()
-
-  print '\nUpdated Access Permissions to the group\n'
-  pprint.pprint(g1)
-
-
 def main(argv):
   """Migrates email messages from Mailman mbox archives to Google Group using 
      the Groups Migration API."""
@@ -111,10 +73,15 @@ def main(argv):
   parser.add_argument('--group',
                       help='Group email address',
                       required=True)
-  parser.add_argument('--archive',
+  parser.add_argument('--mbox',
                       help='Mailman archive file (.mbox)',
                       required=True)
   args = parser.parse_args()
+  
+  # Setup logging
+  logging.basicConfig()
+  logger = logging.getLogger("migrate")
+  logger.setLevel(getattr(logging, args.logging_level))
 
   settings = {}
 
@@ -137,15 +104,18 @@ def main(argv):
   http = credentials.authorize(http)
 
   # Load the archive file
-  logger = logging.getLogger(__name__)
-  logger.info('Importing mbox file %s...', args.archive)
-  archive = mbox(args.archive)
+  logger.info('Importing mbox file %s...', args.mbox)
+  messages = mbox(args.mbox)
 
-  logger.debug('%s contains %d messages.', len(archive))
+  logger.info('%s contains %d messages.', args.mbox, len(messages))
 
-  #service = build('groupssettings', 'v1', http=http)
-
-  #access_settings(service=service, groupId=options.groupId, settings=settings)
+  service = build('groupsmigration', 'v1', http=http)
+  archive = service.archive()
+  
+  for msg in messages:
+    logger.info('Uploading "%s"...', msg['subject'])
+    import pdb; pdb.set_trace()
+    archive.insert(groupId=args.group, body=msg.as_string())  
 
 if __name__ == '__main__':
   main(sys.argv)
